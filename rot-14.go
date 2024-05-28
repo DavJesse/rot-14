660f20556fd236957f2718418db2df4a15c1ee9b
davodhiambo <jesseomolo@yahoo.com>
2024-05-28 16:47:02 +0300

    feature: add rot14 function

--- rot-14.go
+++ rot-14.go
@@ -1,2 +1,13 @@
 package main
 
+func rot14(str string) string {
+	var result int
+
+	for _, r := range str {
+		if r >= 'a' && r <= 'z' || r >= 'A' && r <= 'Z' {
+			if r >= 'a' && r <= 'z' {
+				result += r + 14
+			}
+		}
+	}
+}
