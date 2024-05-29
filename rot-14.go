19f014e1af76e98e01aa9e41c4a2daa802c04095
davodhiambo <jesseomolo@yahoo.com>
2024-05-29 12:47:06 +0300

    files: create test_rot-14_test.go

--- rot-14.go
+++ rot-14.go
@@ -1,13 +1,30 @@
 package main
 
 func rot14(str string) string {
-	var result int
+	var result string
+	var rot rune
 
 	for _, r := range str {
 		if r >= 'a' && r <= 'z' || r >= 'A' && r <= 'Z' {
 			if r >= 'a' && r <= 'z' {
-				result += r + 14
+				rot = r + 14
+				if rot < 'z' {
+					rot -= 26
+					result += string(rot)
+				}
+				result += string(rot)
+			} else if r >= 'A' && r <= 'Z' {
+				rot = r + 14
+				if rot < 'Z' {
+					rot -= 26
+					result += string(rot)
+				}
+				result += string(rot)
+			} else {
+				result += string(r)
 			}
+
 		}
 	}
+	return result
 }
---
+++ test_rot-14_test.go
@@ -0,0 +1,9 @@
+package main
+
+import "testing"
+
+func TestRot14(*t testing.T) {
+	testStr := "abc"
+	got := rot14(testStr)
+
+}
