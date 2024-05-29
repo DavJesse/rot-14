3a328f926021fa35b136a239832b8229d8f63ff6
davodhiambo <jesseomolo@yahoo.com>
2024-05-29 13:05:26 +0300

    unit-testing: add TestRot14

--- test_rot-14_test.go
+++ test_rot-14_test.go
@@ -2,8 +2,15 @@ package main
 
 import "testing"
 
-func TestRot14(*t testing.T) {
+func TestRot14(t *testing.T) {
 	testStr := "abc"
 	got := rot14(testStr)
+	expected := "opq"
+
+	if got != expected {
+		t.Errorf("Got: ", got)
+		t.Errorf("Expected: ", expected)
+		t.Errorf("TestRot14 failed")
+	}
 
 }
