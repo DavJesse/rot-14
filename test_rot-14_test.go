38931a269b4537054913f86c1b8b5b6ed3188546
davodhiambo <jesseomolo@yahoo.com>
2024-05-30 18:57:13 +0300

    unit testing: add test for FromRot14

--- test_rot-14_test.go
+++ test_rot-14_test.go
@@ -2,10 +2,22 @@ package main
 
 import "testing"
 
-func TestRot14(t *testing.T) {
+func TestToRot14(t *testing.T) {
 	testStr := "Hello! How are You?"
 	got := toRot14(testStr)
 	expected := "Vszzc! Vck ofs Mci?"
+
+	if got != expected {
+		t.Errorf("Got: %s", got)
+		t.Errorf("Expected: %s", expected)
+		t.Errorf("TestRot14 failed")
+	}
+}
+
+func TestFromRot14(t *testing.T) {
+	testStr := "Vszzc! Vck ofs Mci?"
+	got := toRot14(testStr)
+	expected := "Hello! How are You?"
 
 	if got != expected {
 		t.Errorf("Got: %s", got)
