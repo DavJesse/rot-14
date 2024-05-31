9b7b59ed735e1b336833dd9da2c942ca6a567d19
davodhiambo <jesseomolo@yahoo.com>
2024-05-31 12:53:04 +0300

    feature: Add support for ROT-14 encoding and decoding flags in main.go.

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 
 func main() {
 	var rotStr string
+	var inputStr string
+	var flag string
 
 	if len(os.Args) != 3 {
 		printLn("This program only takes one argument as input")
@@ -16,10 +18,24 @@ func main() {
 
 	arg := os.Args[1:3]
 
-	if arg[0] == "--to-rot14" {
-		rotStr = toRot14(arg[1])
-	} else if arg[1] == "--to-rot14" {
+	if stringContains(arg[0], "-rot14") {
+		flag = arg[0]
+		inputStr = arg[1]
+		if stringContains(flag, "to") {
+			rotStr = toRot14(inputStr)
+		}
+	} else if stringContains(arg[1], "-rot14") {
+		flag = arg[1]
+		inputStr = arg[0]
+		if stringContains(flag, "from") {
+			rotStr = fromRot14(inputStr)
+		}
 
+	} else {
+		printLn("Please include a '--to-rot14' or '--from-rot14' flag alongside your input string")
+		printLn("Ex. \"go run . --to-rot14 \"Your input string\"")
+		printLn("Ex. \"go run . --from-rot14 \"Your input string\"")
+		return
 	}
 
 	printLn(rotStr)
