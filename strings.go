b8d96edb0085e8e7906cbdcfbf6ef65d78fc51c6
davodhiambo <jesseomolo@yahoo.com>
2024-05-31 12:25:36 +0300

    refactor: Simplify stringContains function and update corresponding test.

--- strings.go
+++ strings.go
@@ -4,12 +4,11 @@ func stringContains(str, subStr string) bool {
 	var status bool
 
 	for i := 0; i < len(str); i++ {
-		for j := i + len(subStr) + 1; j < len(str); j++ {
-			if str[i:j] == subStr {
-				status = true
-				break
-			}
+		if str[i:i+len(subStr)] == subStr {
+			status = true
+			break
 		}
+
 	}
 	return status
 }
