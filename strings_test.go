4444a194b3d3534b51a521d63e8806009bb6d7d8
davodhiambo <jesseomolo@yahoo.com>
2024-05-31 10:23:39 +0300

    unit-testing: Add test for stringContains function.

--- strings_test.go
+++ strings_test.go
@@ -3,4 +3,13 @@ package main
 import "testing"
 
 func TestStringContains(t *testing.T) {
+	testStr1, testSubStr := "Contains", "ins"
+	got := stringContains(testStr1, testSubStr)
+	expected := true
+
+	if got != expected {
+		t.Errorf("Got: %t", got)
+		t.Errorf("Expected: %t", expected)
+		t.Errorf("TestStringContains Failed!")
+	}
 }
