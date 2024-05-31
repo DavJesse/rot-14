881a8bd0f8c0450e80c2d53decd18a96c6a3aff4
davodhiambo <jesseomolo@yahoo.com>
2024-05-31 10:08:08 +0300

    feature: Add stringContains function for substring detection.

--- strings.go
+++ strings.go
@@ -1,4 +1,15 @@
 package main
 
-func stringContains(str string) bool {
+func stringContains(str, subStr string) bool {
+	var status bool
+
+	for i := 0; i < len(str); i++ {
+		for j := i + len(subStr) + 1; j < len(str); j++ {
+			if str[i:j] == subStr {
+				status = true
+				break
+			}
+		}
+	}
+	return status
 }
