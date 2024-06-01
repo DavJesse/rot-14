12f82b58a5f796265f77ffe91d10967427142c06
davodhiambo <jesseomolo@yahoo.com>
2024-06-01 16:29:30 +0300

    files: add comments to strings_test.go

--- strings_test.go
+++ strings_test.go
@@ -2,11 +2,18 @@ package main
 
 import "testing"
 
+// TestStringContains tests the stringContains function.
 func TestStringContains(t *testing.T) {
+	// Define test input values
 	testStr1, testSubStr := "Contains", "ins"
+
+	// Call stringContains for testing
 	got := stringContains(testStr1, testSubStr)
+
+	// Define the expected output
 	expected := true
 
+	// Check if the output matches the expected result
 	if got != expected {
 		t.Errorf("Got: %t", got)
 		t.Errorf("Expected: %t", expected)
