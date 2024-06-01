b9183fb7bf2b92249421665fc01058a5897fdd37
davodhiambo <jesseomolo@yahoo.com>
2024-06-01 16:04:42 +0300

    files: add comments to strings.go

--- strings.go
+++ strings.go
@@ -4,6 +4,7 @@ func stringContains(str, subStr string) bool {
 	lenStr := len(str)
 	lenSubStr := len(subStr)
 
+	// Exit function when substring is incaompatible(is longer)
 	if lenStr < lenSubStr {
 		return false
 	}
@@ -11,6 +12,7 @@ func stringContains(str, subStr string) bool {
 	for i := 0; i < lenStr; i++ {
 		j := i + lenSubStr
 
+		// Compare chunks of str to substring
 		if j <= lenStr && str[i:j] == subStr {
 			return true
 		}
